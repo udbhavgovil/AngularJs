6bc3261dfde7f82eeaf90cda2f36adeedea994b4
udbhavgovil <udbhavgovil@gmail.com>
2016-09-23 00:40:23 +0530

    Update app.js

--- week1-sol/app.js
+++ week1-sol/app.js
@@ -18,6 +18,37 @@ angular.module('LunchCheck',[])
 			var items = $scope.Items.split(separtor);
 			//console.log(items.length);
 			//console.log('okay')
+<<<<<<< HEAD
+			if ($scope.Items.length==0)
+				$scope.message = "Enter lunch items first!";
+			else if (items.length<=3)
+=======
+			
+			if (items.length<=3)
+>>>>>>> cee3f9c53d9d02a0a73e3a5f5bfefa2e833db209
+				$scope.message = "Enjoy!";
+			else
+				$scope.message = "Too Much!";
+			}
+		};
+	}
+	//console.log("ok");
+})();
+
+
+
+	function LunchCheckController($scope) {
+
+		$scope.Items = "" ;
+		$scope.message = "" ;
+		$scope.CountItem = function (){
+			var separtor = ','
+			if ($scope.Items.length==0)
+				$scope.message = "Enter lucnh items first!";
+			else {
+			var items = $scope.Items.split(separtor);
+			//console.log(items.length);
+			//console.log('okay')
 			
 			if (items.length<=3)
 				$scope.message = "Enjoy!";
